ff5a08574616d913e38e3bc97124b11a91e42394
bomul0304 <bomulphs@naver.com>
2024-07-24 22:11:56 +0900

    화살표 없애기

---
+++ domino/lib/provider/TD/date_provider.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class DateProvider extends ChangeNotifier {
+  DateTime _pickedDate = DateTime.now();
+  DateTime get pickedDate => _pickedDate;
+
+  bool _everyDay = false;
+  bool _everyWeek = false;
+  bool _everyTwoWeek = false;
+  bool _everyMonth = false;
+  int _interval = 0;
+
+  bool get everyDay => _everyDay;
+  bool get everyWeek => _everyWeek;
+  bool get everyTwoWeek => _everyTwoWeek;
+  bool get everyMonth => _everyMonth;
+  int get interval => _interval;
+
+  void setPickedDate(DateTime date) {
+    _pickedDate = date;
+    notifyListeners(); // 상태 변경 알림
+  }
+
+  void setEveryday(bool everyDay) {
+    _everyDay = everyDay;
+    _everyWeek = false;
+    _everyTwoWeek = false;
+    _everyMonth = false;
+    notifyListeners();
+  }
+
+  void setEveryweek(bool everyWeek) {
+    _everyWeek = everyWeek;
+    _everyDay = false;
+    _everyTwoWeek = false;
+    _everyMonth = false;
+    notifyListeners();
+  }
+
+  void setEverytwoweek(bool everyTwoWeek) {
+    _everyTwoWeek = everyTwoWeek;
+    _everyDay = false;
+    _everyWeek = false;
+    _everyMonth = false;
+    notifyListeners();
+  }
+
+  void setEverymonth(bool everyMonth) {
+    _everyMonth = everyMonth;
+    _everyDay = false;
+    _everyWeek = false;
+    _everyTwoWeek = false;
+    notifyListeners();
+  }
+
+  void setInterval(bool repeat) {
+    _interval = 0;
+    if (repeat) {
+      if (_everyDay) {
+        _interval = 1;
+      }
+      if (_everyWeek) {
+        _interval = 7;
+      }
+      if (_everyTwoWeek) {
+        _interval = 14;
+      }
+      if (_everyMonth) {
+        // 현재 선택된 날짜의 월과 일(day)을 기억
+        int currentMonth = _pickedDate.month;
+        int currentDay = _pickedDate.day;
+
+        // 다음 달로 이동
+        int nextMonth = currentMonth + 1;
+        int nextYear = _pickedDate.year;
+
+        if (nextMonth > 12) {
+          nextMonth = 1;
+          nextYear++;
+        }
+
+        // 다음 달의 같은 일로 설정
+        _interval = DateTime(nextYear, nextMonth, currentDay)
+            .difference(_pickedDate)
+            .inDays;
+      }
+    }
+
+    notifyListeners();
+  }
+
+  void updateRepeatSettings() {
+    if (_interval == 1) {
+      _everyDay = true;
+      _everyWeek = false;
+      _everyTwoWeek = false;
+      _everyMonth = false;
+    } else if (_interval == 7) {
+      _everyDay = false;
+      _everyWeek = true;
+      _everyTwoWeek = false;
+      _everyMonth = false;
+    } else if (_interval == 14) {
+      _everyDay = false;
+      _everyWeek = false;
+      _everyTwoWeek = true;
+      _everyMonth = false;
+    } else if (_interval > 14) {
+      _everyDay = false;
+      _everyWeek = false;
+      _everyTwoWeek = false;
+      _everyMonth = true;
+    } else {
+      _everyDay = false;
+      _everyWeek = false;
+      _everyTwoWeek = false;
+      _everyMonth = false;
+    }
+    notifyListeners();
+  }
+}
---
+++ domino/lib/screens/DP/create_select_page.dart
@@ -0,0 +1,187 @@
+import 'package:domino/screens/DP/create99_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:domino/provider/DP/model.dart';
+import 'package:domino/widgets/DP/smallgrid.dart';
+
+class DPcreateSelectPage extends StatelessWidget {
+  const DPcreateSelectPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xff262626),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: const Color(0xff262626),
+          title: Padding(
+            padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+            child: Text(
+              '플랜 만들기',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: MediaQuery.of(context).size.width * 0.06,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.fromLTRB(38.0, 20.0, 40.0, 0.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("어떤 목표를 이루고 싶나요?",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 19,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1.1)),
+
+                const SizedBox(height: 20),
+
+                Container(
+                  height: 43,
+                  padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(3),
+                    border: Border.all(
+                      color: const Color(0xff5C5C5C),
+                    ),
+                  ),
+                  child: const MyDropdownButton(),
+                ),
+
+                const SizedBox(
+                  height: 20,
+                ),
+
+                Expanded(
+                  child: GridView(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3, crossAxisSpacing: 1, mainAxisSpacing: 1),
+                    children: List.generate(9, (index) {
+                      if (index == 4) {
+                        return SizedBox(
+                            width: 100,
+                            child: GridView.count(
+                                crossAxisCount: 3,
+                                crossAxisSpacing: 1,
+                                mainAxisSpacing: 1,
+                                children: List.generate(9, (index) {
+                                  return Container(
+                                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(3),
+                                    color: index == 4
+                                        ? const Color(0xffFCFF62)
+                                        : const Color(0xff929292),),
+                                    alignment: Alignment.center,
+                                    
+                                    margin: const EdgeInsets.all(1.0),
+                                    child: index == 4
+                                        ? Consumer<SelectFinalGoalModel>(
+                                            builder: (_, selectedFinalGoal,
+                                                    __) =>
+                                                Text(
+                                                  selectedFinalGoal
+                                                      .selectedFinalGoal,
+                                                  style: const TextStyle(
+                                                      color: Colors.black,
+                                                      fontSize: 8,
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                  textAlign: TextAlign.center,
+                                                ))
+                                        : const Text(""),
+                                  );
+                                })));
+                      } else {
+                        return const smallgrid();
+                      }
+                    }),
+                  ),
+                ),
+                
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: TextButton.styleFrom(
+                            backgroundColor: const Color(0xff131313),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6.0))),
+                        child: const Text(
+                          '취소',
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const DPcreate99Page(),
+                              ));
+                        },
+                        style: TextButton.styleFrom(
+                            backgroundColor: const Color(0xff131313),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6.0))),
+                        child: const Text(
+                          '다음',
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                      )
+                    ]),
+
+                    const SizedBox(
+                  height: 20,
+                ),
+              ],
+            )));
+  }
+}
+
+const List<String> list = <String>["선택 안됨", "환상적인 세계여행", "행복한 2024년"];
+
+class MyDropdownButton extends StatefulWidget {
+  const MyDropdownButton({super.key});
+
+  @override
+  State<MyDropdownButton> createState() => _MyDropdownButtonState();
+}
+
+class _MyDropdownButtonState extends State<MyDropdownButton> {
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return (DropdownButton<String>(
+      value: dropdownValue,
+      onChanged: (String? value) {
+        context.read<SelectFinalGoalModel>().selectFinalGoal(value!);
+        setState(() {
+          dropdownValue = value;
+        });
+      },
+      items: list.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      isExpanded: true,
+      elevation: 1,
+      dropdownColor: const Color(0xff262626),
+      underline: Container(),
+      icon: const Icon(Icons.arrow_drop_down),
+      iconEnabledColor: Colors.white,
+      style: const TextStyle(
+        color: Colors.white,
+      ),
+    ));
+  }
+}
