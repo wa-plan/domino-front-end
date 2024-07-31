0160d797e19541a846fa15f80caa705974274520
bomul0304 <bomulphs@naver.com>
2024-07-31 16:03:21 +0900

    DP GUI 수정

---
+++ lib/widgets/DP/confirmBox.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:domino/provider/DP/model.dart';
+
+class confirmBox extends StatelessWidget {
+  
+  
+  final int actionPlanid;
+  final int detailGoalid;
+
+  const confirmBox({super.key, required this.actionPlanid, required this.detailGoalid});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SizedBox(
+                    width: 100,
+                    child: GridView.count(
+                      crossAxisCount: 3,
+                      crossAxisSpacing: 1,
+                      mainAxisSpacing: 1,
+                      children: List.generate(9, (index) {
+
+                        final inputtedActionPlan = context.watch<SaveInputtedActionPlanModel>().inputtedActionPlan[actionPlanid];
+                        final values = inputtedActionPlan.containsKey(index.toString()) ? inputtedActionPlan[index.toString()] : '';
+                        final detailGoal = context.watch<SaveInputtedDetailGoalModel>().inputtedDetailGoal['$detailGoalid'];
+                        final backgroundColor1 = detailGoal.isEmpty ? const Color(0xff262626) : const Color(0xff929292);
+
+                        if (index == 4) {
+                            return Container(
+                              
+                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(3),
+                              color: backgroundColor1,),
+                              margin: const EdgeInsets.all(1.0),
+                              child: Center(
+                                child:Text(
+                                  detailGoal,
+                                  style: const TextStyle(color: Colors.black, fontSize: 15),
+                                  textAlign: TextAlign.center,
+                                )),
+                              
+                            );
+                          } else {
+                            final isValueEmpty = values.isEmpty;
+                            final backgroundColor2 = isValueEmpty ? const Color(0xff262626) : const Color(0xff5C5C5C);
+
+                            return Container(
+                             
+                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(3),
+                               color: backgroundColor2,),
+                              margin: const EdgeInsets.all(1.0),
+                              child: Center(
+                                child:Text(
+                                values.toString(),
+                                style: const TextStyle(color: Colors.black, fontSize: 15),
+                                textAlign: TextAlign.center,
+                              )),
+                            );
+                          }
+                      })
+                    )
+                  );
+  }
+}
--- lib/widgets/DP/smallgrid.dart
+++ lib/widgets/DP/smallgrid.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
-class Smallgrid extends StatelessWidget {
-  const Smallgrid({super.key});
+
+class smallgrid extends StatelessWidget {
+  const smallgrid({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: 100,
-        child: GridView.count(
-            crossAxisCount: 3,
-            children: List.generate(9, (index) {
-              return Container(
-                color: index == 4
-                    ? const Color(0xff929292)
-                    : const Color(0xff5C5C5C),
-                margin: const EdgeInsets.all(1.0),
-              );
-            })));
+                    width: 100,
+                    child: GridView.count(
+                      crossAxisCount: 3,
+                      crossAxisSpacing: 1,
+                      mainAxisSpacing: 1,
+                      children: List.generate(9, (index) {
+                        return Container(
+                          
+                          margin: const EdgeInsets.all(1.0),
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(3),
+                          color: index == 4 ? const Color(0xff929292) : const Color(0xff5C5C5C),
+                          ),
+                          
+                        );
+                      })
+                    )
+                  );
   }
 }
