1222286d58c5edeebbbe1d93f9cc5b950e4b349a
bomul0304 <bomulphs@naver.com>
2024-08-02 14:45:45 +0900

    add page 1에 만다라트 위젯 추가

--- lib/apis/api_function.dart
+++ lib/apis/api_function.dart
@@ -1,5 +1,5 @@
 import 'package:http/http.dart' as http;
-import 'package:domino/apis/DP/api_model.dart';
+import 'package:domino/apis/api_model.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class Services {
