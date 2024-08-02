0bf05469e6c78fc37abcd3cba36ef159ce0e82c9
chaemd <127921476+chaemd@users.noreply.github.com>
2024-08-02 14:14:34 +0900

    Update member_register_page.dart

--- lib/screens/LR/member_register_page.dart
+++ lib/screens/LR/member_register_page.dart
@@ -1,41 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:domino/provider/LR/user_provider.dart';
 
-class UserProvider extends ChangeNotifier {
-  String _email = ""; 
-  String _password = "";
-
-  String get email => _email;
-  String get password => _password;
-
-  void set email(String input_email) {
-    _email = input_email;
-    notifyListeners();
-  }
-
-  void set password(String input_password) {
-    _password = input_password;
-    notifyListeners();
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+/*class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-        create: (_) => UserProvider(),
-        child: MaterialApp(
-            title: 'Domino',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(primarySwatch: Colors.orange),
-            routes: {
-              '/login': (BuildContext context) => LoginWidget(),
-              '/main': (BuildContext context) => MainGridView(),
-              '/enrollPassword': (BuildContext context) =>
-                  EnrollPasswordWidget()
-            },
-            home: LoginWidget()));
+    return LoginWidget();
   }
 }
 
@@ -55,4 +26,4 @@ _userProvider.email = _emailConroller.text;
     _userProvider.email = _emailConroller.text;
     Navigator.push(context,
         MaterialPageRoute(builder: ((context) => EnrollPasswordWidget())));
-  }
+  }*/
