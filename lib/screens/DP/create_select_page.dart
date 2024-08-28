19d611186ef6738bb860d89b7146c1aac34665fc
bomul0304 <118335212+bomul0304@users.noreply.github.com>
2024-08-28 14:32:13 +0900

    DP api

--- lib/screens/DP/create_select_page.dart
+++ lib/screens/DP/create_select_page.dart
@@ -13,195 +13,216 @@ class DPcreateSelectPage extends StatefulWidget {
 }
 
 class _DPcreateSelectPageState extends State<DPcreateSelectPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: const Color(0xff262626),
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: const Color(0xff262626),
-          title: Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
-            child: Text(
-              '플랜 만들기',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: MediaQuery.of(context).size.width * 0.06,
-                  fontWeight: FontWeight.w600),
-            ),
-          ),
-        ),
-        body: Padding(
-            padding: const EdgeInsets.fromLTRB(38.0, 20.0, 40.0, 0.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("어떤 목표를 이루고 싶나요?",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 19,
-                        fontWeight: FontWeight.w600,
-                        letterSpacing: 1.1)),
-                const SizedBox(height: 20),
-                Container(
-                  height: 43,
-                  padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(3),
-                    border: Border.all(
-                      color: const Color(0xff5C5C5C),
-                    ),
-                  ),
-                  child: const MyDropdownButton(),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Expanded(
-                  child: GridView(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3,
-                            crossAxisSpacing: 1,
-                            mainAxisSpacing: 1),
-                    children: List.generate(9, (index) {
-                      if (index == 4) {
-                        return SizedBox(
-                            width: 100,
-                            child: GridView.count(
-                                crossAxisCount: 3,
-                                crossAxisSpacing: 1,
-                                mainAxisSpacing: 1,
-                                children: List.generate(9, (index) {
-                                  return Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(3),
-                                      color: index == 4
-                                          ? const Color(0xffFCFF62)
-                                          : const Color(0xff929292),
-                                    ),
-                                    alignment: Alignment.center,
-                                    margin: const EdgeInsets.all(1.0),
-                                    child: index == 4
-                                        ? Consumer<SelectFinalGoalModel>(
-                                            builder: (_, selectedFinalGoal,
-                                                    __) =>
-                                                Text(
-                                                  selectedFinalGoal
-                                                      .selectedFinalGoal,
-                                                  style: const TextStyle(
-                                                      color: Colors.black,
-                                                      fontSize: 8,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                  textAlign: TextAlign.center,
-                                                ))
-                                        : const Text(""),
-                                  );
-                                })));
-                      } else {
-                        return const Smallgrid();
-                      }
-                    }),
-                  ),
-                ),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        style: TextButton.styleFrom(
-                            backgroundColor: const Color(0xff131313),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(6.0))),
-                        child: const Text(
-                          '취소',
-                          style: TextStyle(color: Colors.white, fontSize: 15),
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const DPcreate99Page(),
-                              ));
-                        },
-                        style: TextButton.styleFrom(
-                            backgroundColor: const Color(0xff131313),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(6.0))),
-                        child: const Text(
-                          '다음',
-                          style: TextStyle(color: Colors.white, fontSize: 15),
-                        ),
-                      )
-                    ]),
-                const SizedBox(
-                  height: 20,
-                ),
-              ],
-            )));
-  }
-}
-
-class MyDropdownButton extends StatefulWidget {
-  const MyDropdownButton({super.key});
-
-  @override
-  State<MyDropdownButton> createState() => _MyDropdownButtonState();
-}
-
-class _MyDropdownButtonState extends State<MyDropdownButton> {
-  List<String> dropdownItems = <String>["선택 안됨", "환상적인 세계여행", "행복한 2024년"];
-  String dropdownValue = "선택 안됨"; //기본값 설정
-  //String dropdownValue = list.first;
-  //List<String> dropdownItems = list;
-
-  /*void mainGoalList() async {
-    List<String>? serverGoals = await MainGoalListService.mainGoalList(context);
-    if (serverGoals != null) {
-      setState(() {
-        dropdownItems = ["선택 안됨", ...serverGoals];
-        dropdownValue = dropdownItems.first;
-      });
-    }
-  }*/
+  String? selectedGoalId;
+  String selectedGoalName = '';
+  List<Map<String, dynamic>> mainGoals = [];
 
   @override
   void initState() {
     super.initState();
-    //mainGoalList(); //초기화 시 서버 데이터 불러오기
+    _mainGoalList();
+  }
+
+  void _mainGoalList() async {
+    List<Map<String, dynamic>>? goals =
+        await MainGoalListService.mainGoalList(context);
+    if (goals != null) {
+      setState(() {
+        mainGoals = goals;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return (DropdownButton<String>(
-      value: dropdownValue,
-      onChanged: (String? value) {
-        //context.read<SelectFinalGoalModel>().selectFinalGoal(value!);
-        setState(() {
-          dropdownValue = value!;
-        });
-      },
-      items: dropdownItems.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-      isExpanded: true,
-      elevation: 1,
-      dropdownColor: const Color(0xff262626),
-      underline: Container(),
-      icon: const Icon(Icons.arrow_drop_down),
-      iconEnabledColor: Colors.white,
-      style: const TextStyle(
-        color: Colors.white,
+    return Scaffold(
+      backgroundColor: const Color(0xff262626),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color(0xff262626),
+        title: Padding(
+          padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+          child: Text(
+            '플랜 만들기',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: MediaQuery.of(context).size.width * 0.06,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
       ),
-    ));
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(38.0, 20.0, 40.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "어떤 목표를 이루고 싶나요?",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 19,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1.1,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              height: 43,
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(3),
+                border: Border.all(
+                  color: const Color(0xff5C5C5C),
+                ),
+              ),
+              child: FutureBuilder(
+                future: MainGoalListService.mainGoalList(context),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return const Center(
+                      child: Text(
+                        '목표를 불러오는 데 실패했습니다.',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    );
+                  } else if (snapshot.hasData) {
+                    return DropdownButton<String>(
+                      value: selectedGoalId,
+                      items: snapshot.data!
+                          .map<DropdownMenuItem<String>>((goal) {
+                        final goalName = goal['name'] ?? 'Unknown Goal';
+                        return DropdownMenuItem<String>(
+                          value: goal['id'].toString(),
+                          child: Text(
+                            goalName,
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String? value) {
+                        final selectedGoal = snapshot.data!.firstWhere(
+                            (goal) => goal['id'].toString() == value);
+                        setState(() {
+                          selectedGoalId = value ?? '';
+                          selectedGoalName = selectedGoal['name'] ?? '';
+                        });
+                        context.read<SelectFinalGoalModel>().selectFinalGoal(selectedGoalName);
+                        context.read<SelectFinalGoalId>().selectFinalGoalId(selectedGoalId!);
+                      },
+                      isExpanded: true,
+                      dropdownColor: const Color(0xff262626),
+                      style: const TextStyle(color: Colors.white),
+                      iconEnabledColor: Colors.white,
+                      underline: Container(),
+                    );
+                  } else {
+                    return const Center(
+                      child: Text(
+                        '목표가 없습니다.',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: GridView(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 1,
+                  mainAxisSpacing: 1,
+                ),
+                children: List.generate(9, (index) {
+                  if (index == 4) {
+                    return SizedBox(
+                      width: 100,
+                      child: GridView.count(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 1,
+                        mainAxisSpacing: 1,
+                        children: List.generate(9, (innerIndex) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(3),
+                              color: innerIndex == 4
+                                  ? const Color(0xffFCFF62)
+                                  : const Color(0xff929292),
+                            ),
+                            alignment: Alignment.center,
+                            margin: const EdgeInsets.all(1.0),
+                            child: innerIndex == 4
+                                ? Text(
+                                    selectedGoalName,
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 8,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  )
+                                : const Text(""),
+                          );
+                        }),
+                      ),
+                    );
+                  } else {
+                    return const Smallgrid();
+                  }
+                }),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0xff131313),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6.0),
+                    ),
+                  ),
+                  child: const Text(
+                    '취소',
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const DPcreate99Page(),
+                      ),
+                    );
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0xff131313),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6.0),
+                    ),
+                  ),
+                  child: const Text(
+                    '다음',
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
   }
 }
