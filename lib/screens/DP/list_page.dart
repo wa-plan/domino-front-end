679d419b81d9550ebf893d60ca471108479ec141
bomul0304 <118335212+bomul0304@users.noreply.github.com>
2024-08-28 17:08:48 +0900

    Update list_page.dart

--- lib/screens/DP/list_page.dart
+++ lib/screens/DP/list_page.dart
@@ -1,190 +1,341 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:domino/screens/DP/create_select_page.dart';
-import 'package:domino/widgets/nav_bar.dart';
-import 'package:domino/apis/services/dp_services.dart'; 
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class DPlistPage extends StatefulWidget {
-  const DPlistPage({super.key});
+class MainGoalListService {
+  static Future<List<Map<String, dynamic>>?> mainGoalList(
+    BuildContext context,
+  ) async {
+    final prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('authToken');
+    print('저장된 토큰: $token');
 
-  @override
-  State<DPlistPage> createState() => _DPlistPageState();
-}
+    if (token == null) {
+      Fluttertoast.showToast(
+        msg: '로그인 토큰이 없습니다. 다시 로그인해 주세요.',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+      return null;
+    }
 
-class _DPlistPageState extends State<DPlistPage> {
+    final url = Uri.parse('http://13.124.78.26:8080/api/mandalart');
 
-  List<Map<String, dynamic>> mainGoals = [];
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
 
-  @override
-  void initState() {
-    super.initState();
-    _mainGoalList();
-  }
+      print('서버 응답 상태 코드: ${response.statusCode}');
+      print('서버 응답: ${response.body}');
 
-  void _mainGoalList() async {
-    List<Map<String, dynamic>>? goals =
-        await MainGoalListService.mainGoalList(context);
-    if (goals != null) {
-      setState(() {
-        mainGoals = goals;
-      });
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonResponse = json.decode(response.body);
+        List<Map<String, dynamic>> mainGoals = jsonResponse
+            .map((item) => {
+                  'id': item['id'],
+                  'name': item['name'],
+                })
+            .toList();
+        print('mainGoals: $mainGoals');
+        return mainGoals;
+      } else {
+        Fluttertoast.showToast(
+          msg: '업데이트 실패: ${response.body}',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+        );
+        return null;
+      }
+    } catch (e) {
+      Fluttertoast.showToast(
+        msg: '오류 발생: $e',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+      return null;
     }
   }
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xff262626),
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: const Color(0xff262626),
-        title: Padding(
-          padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
-          child: Text(
-            '도미노 플랜',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: MediaQuery.of(context).size.width * 0.06,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ),
-      ),
-      bottomNavigationBar: const NavBar(),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(38.0, 30.0, 40.0, 0.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                PopupMenuButton(
-                  iconColor: const Color(0xff5C5C5C),
-                  itemBuilder: (context) {
-                    return [
-                      const PopupMenuItem(
-                        value: 'edit',
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            '삭제하기',
-                            style: TextStyle(fontWeight: FontWeight.w600),
-                          ),
-                        ),
-                      ),
-                      const PopupMenuItem(
-                        value: 'delete',
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            '수정하기',
-                            style: TextStyle(fontWeight: FontWeight.w600),
-                          ),
-                        ),
-                      ),
-                    ];
-                  },
-                  onSelected: (value) {
-                    switch (value) {
-                      case 'edit':
-                        print('edit');
-                        break;
-                      case 'delete':
-                        print('delete');
-                        break;
-                    }
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(Icons.add),
-                  color: const Color(0xff5C5C5C),
-                  iconSize: 35.0,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const DPcreateSelectPage(),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-            
-            
-            const SizedBox(height: 20),
-            Expanded(
-              child: PageView.builder(
-                itemCount: mainGoals.length,
-                itemBuilder: (context, index) {
-                  final goal = mainGoals[index];
-                  final mandalartId = goal['id'].toString();
-                  return FutureBuilder<List<Map<String, dynamic>>?>(
-                future: SecondGoalListService.secondGoalList(context, mandalartId),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return const Center(
-                      child: Text(
-                        '데이터를 불러오는 데 실패했습니다.',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    );
-                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                    return const Center(
-                      child: Text(
-                        '목표가 없습니다.',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    );
-                  } else {
-                    final data = snapshot.data!;
-                    return ListView.builder(
-                      itemCount: data.length,
-                      itemBuilder: (context, index) {
-                        final item = data[index];
-                        final mandalart = item['mandalart'];
-                        final secondGoals = item['secondGoals'] as List;
+class AddSecondGoalService {
+  static Future<bool> addSecondGoal({
+    required String mandalartId,
+    required List<String> name,
+    required List<String> color,
+  }) async {
+    final prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('authToken');
+    print('저장된 토큰: $token');
 
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: secondGoals.map<Widget>((goal) {
-                            final thirdGoals = goal['thirdGoals'] as List;
+    if (token == null) {
+      Fluttertoast.showToast(
+        msg: '로그인 토큰이 없습니다. 다시 로그인해 주세요.',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+      return false;
+    }
 
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 8.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    '- $mandalart: ${goal['secondGoal']}',
-                                    style: const TextStyle(color: Colors.white),
-                                  ),
-                                  ...thirdGoals.map<Widget>((thirdGoal) {
-                                    return Padding(
-                                      padding: const EdgeInsets.only(left: 16.0, top: 4.0),
-                                      child: Text(
-                                        '-- ${thirdGoal['thirdGoal']}',
-                                        style: const TextStyle(color: Colors.grey),
-                                      ),
-                                    );
-                                  }),
-                                ],
-                              ),
-                            );
-                          }).toList(),
-                        );
-                      },
-                    );
-                  }
-                },
-              );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    final url = Uri.parse('http://13.124.78.26:8080/api/secondgoal/add');
+
+    if (name.length != color.length) {
+      throw Exception("Mismatch in the number of names and colors");
+    }
+
+    bool allSuccess = true;
+
+    for (int i = 0; i < name.length; i++) {
+      final body = json.encode({
+        "mandalartId": mandalartId,
+        "name": name[i],
+        "color": color[i]
+      });
+
+      try {
+        final response = await http.post(
+          url,
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token',
+          },
+          body: body,
+        );
+
+        print('서버 응답 상태 코드: ${response.statusCode}');
+        print('서버 응답 본문: ${response.body}');
+
+        if (response.statusCode == 200 || response.statusCode == 201) {
+          Fluttertoast.showToast(
+            msg: '목표가 성공적으로 저장되었습니다.',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+          );
+        } else {
+          Fluttertoast.showToast(
+            msg: '목표 생성 실패: ${response.body}',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+          );
+          allSuccess = false;
+        }
+      } catch (e) {
+        Fluttertoast.showToast(
+          msg: '오류 발생: $e',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+        );
+        allSuccess = false;
+      }
+    }
+
+    return allSuccess;
+  }
+}
+
+
+
+class SecondGoalListService {
+  static Future<List<Map<String, dynamic>>?> secondGoalList(
+    BuildContext context,
+    String mandalartId, // Added to dynamically insert mandalartId in the URL
+  ) async {
+    final prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('authToken');
+    print('저장된 토큰: $token');
+
+    if (token == null) {
+      Fluttertoast.showToast(
+        msg: '로그인 토큰이 없습니다. 다시 로그인해 주세요.',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+      return null;
+    }
+
+    final url =
+        Uri.parse('http://13.124.78.26:8080/api/mandalart/all/$mandalartId'); // Insert mandalartId here
+
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      print('서버 응답 상태 코드: ${response.statusCode}');
+      print('서버 응답: ${response.body}');
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> jsonResponse = json.decode(response.body);
+        
+        // Parsing the response to match the desired structure
+        final List<Map<String, dynamic>> mainGoals = [
+          {
+            "mandalart": jsonResponse["mandalart"],
+            "secondGoals": (jsonResponse["secondGoals"] as List<dynamic>)
+                .map((secondGoal) => {
+                      "id": secondGoal["id"],
+                      "secondGoal": secondGoal["secondGoal"],
+                      "thirdGoals": (secondGoal["thirdGoals"] as List<dynamic>)
+                          .map((thirdGoal) => {
+                                "id": thirdGoal["id"],
+                                "thirdGoal": thirdGoal["thirdGoal"],
+                              })
+                          .toList(),
+                    })
+                .toList(),
+          }
+        ];
+
+        print('mainGoals: $mainGoals');
+        return mainGoals;
+      } else {
+        Fluttertoast.showToast(
+          msg: '업데이트 실패: ${response.body}',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+        );
+        return null;
+      }
+    } catch (e) {
+      Fluttertoast.showToast(
+        msg: '오류 발생: $e',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+      return null;
+    }
+  }
+}
+
+
+class AddThirdGoalService {
+  static Future<bool> addThirdGoal({
+    required List<int> secondGoalId,
+    required List<String> third0,
+    required List<String> third1,
+    required List<String> third2,
+    required List<String> third3,
+    required List<String> third4,
+    required List<String> third5,
+    required List<String> third6,
+    required List<String> third7,
+    required List<String> third8,
+  }) async {
+    final prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('authToken');
+    print('저장된 토큰: $token');
+
+    if (token == null) {
+      Fluttertoast.showToast(
+        msg: '로그인 토큰이 없습니다. 다시 로그인해 주세요.',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+      return false;
+    }
+
+    final url = Uri.parse('http://13.124.78.26:8080/api/thirdgoal/add');
+
+    bool allSuccess = true;
+
+    // List of all thirdGoals
+    List<List<String>> allThirdGoals = [
+      third0, third1, third2, third3, third4, third5, third6, third7, third8
+    ];
+
+    for (int i = 0; i < secondGoalId.length; i++) {
+      // Check if we have more thirdGoals than secondGoals
+      if (i >= allThirdGoals.length) break;
+
+      List<String> currentThirdGoals = allThirdGoals[i];
+      
+      for (int j = 0; j < currentThirdGoals.length; j++) {
+        final thirdGoalName = currentThirdGoals[j];
+        
+        // Skip empty strings
+        if (thirdGoalName.isEmpty) continue;
+
+        final body = json.encode({
+          "secondGoalId": secondGoalId[i],
+          "name": thirdGoalName,
+        });
+
+        try {
+          final response = await http.post(
+            url,
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer $token',
+            },
+            body: body,
+          );
+
+          print('서버 응답 상태 코드: ${response.statusCode}');
+          print('서버 응답 본문: ${response.body}');
+
+          if (response.statusCode == 200 || response.statusCode == 201) {
+            Fluttertoast.showToast(
+              msg: '목표가 성공적으로 저장되었습니다.',
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              backgroundColor: Colors.green,
+              textColor: Colors.white,
+            );
+          } else {
+            Fluttertoast.showToast(
+              msg: '목표 생성 실패: ${response.body}',
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              backgroundColor: Colors.red,
+              textColor: Colors.white,
+            );
+            allSuccess = false;
+          }
+        } catch (e) {
+          Fluttertoast.showToast(
+            msg: '오류 발생: $e',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+          );
+          allSuccess = false;
+        }
+      }
+    }
+
+    return allSuccess;
   }
 }
