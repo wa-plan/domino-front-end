8977c0de331f8643ae30ea489986455827a3f679
luenechoi <156325695+luenechoi@users.noreply.github.com>
2024-11-24 21:29:53 +0900

    튜토리얼 리뉴얼

---
+++ lib/screens/TUTOR/tutorials.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class Tutorials extends StatefulWidget {
+  const Tutorials({super.key});
+
+  @override
+  State<Tutorials> createState() => _TutorialsState();
+}
+
+class _TutorialsState extends State<Tutorials> {
+  final List<String> _list = [
+    'assets/img/tutor/tutor_1_re.png',
+    'assets/img/tutor/tutor_2_re.png',
+    'assets/img/tutor/tutor_3_re.png',
+    'assets/img/tutor/tutor_4_re.png',
+    'assets/img/tutor/tutor_5_re.png',
+    'assets/img/tutor/tutor_6_re.png',
+    'assets/img/tutor/tutor_7_re.png',
+  ]; //이미지 리스트
+  final CarouselController _controller = CarouselController();
+  //CarouselController();
+  int currentIndex = 0; //인덱스
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CarouselSlider(
+          //carouselController: _controller,
+          options: CarouselOptions(
+            height: double.infinity, // 최대 크기로 지정
+            enableInfiniteScroll: false,
+            viewportFraction: 0.8, // 이미지 100% 비율로 보여줌
+            onPageChanged: ((index, reason) {
+              // 페이지 슬라이드 시 index 변경
+              setState(() {
+                currentIndex = index;
+              });
+            }),
+          ),
+          items: _list.map((String item) {
+            return Image.asset(item, fit: BoxFit.contain); // 이미지를 화면에 맞게 조절
+          }).toList(),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: _list.asMap().entries.map((entry) {
+            return Align(
+              alignment: Alignment.bottomCenter,
+              child: GestureDetector(
+                onTap: () {
+                  // indicator에서 터치한 페이지로 이동
+                  _controller.animateToPage(entry.key);
+                },
+                // indicator에 표시될 위젯
+                child: Container(
+                  width: 12.0,
+                  height: 12.0,
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 45.0, horizontal: 4.0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: const Color.fromARGB(255, 133, 133, 133)
+                          .withOpacity(currentIndex == entry.key
+                              ? 0.9
+                              : 0.4)), // 현재 index 표시
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+        Positioned(
+            bottom: 80.0,
+            left: 0,
+            right: 0,
+            child: Center(
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("다음",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                          color: Colors.black,
+                        )))))
+        Positioned(
+            bottom: 80.0,
+            left: 0,
+            right: 0,
+            child: Center(
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("시작하기",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                          color: Colors.black,
+                        )))))
+      ],
+    );
+  }
+}
+
+class CarouselController {}
+
+extension on CarouselController {
+  void animateToPage(int key) {}
+}
