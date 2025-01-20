cba2d315d359161bb857e9cf3eb67d44074c9f9f
chaemd <new7932@naver.com>
2025-01-21 01:25:01 +0900

    유진님 수정

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ void main() async {
         ChangeNotifierProvider(create: (_) => SaveMandalartCreatedGoal()),
         ChangeNotifierProvider(create: (_) => TestInputtedDetailGoalModel()),
         ChangeNotifierProvider(create: (_) => TestInputtedActionPlanModel()),
+        ChangeNotifierProvider(create: (_) => GoalOrder()),
       ],
       child: const MyApp(), // MyApp 클래스를 사용
     ),
@@ -56,58 +57,44 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'domino',
       debugShowCheckedModeBanner: false,
-      home: const LoginScreen(), 
+      home: const LoginScreen(),
       theme: ThemeData(
-        fontFamily: 'Pretendard',
-        useMaterial3: true,
-        textTheme: TextTheme(
-          // all > appBar의 메뉴 텍스트 
-          titleLarge: TextStyle(
-            fontSize: MediaQuery.of(context).size.width*0.05,
-            fontWeight: FontWeight.w600,
-            color:Colors.white),
-           // all > 메뉴 설명
-          titleMedium: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w600,
-            color:Colors.white
+          fontFamily: 'Pretendard',
+          useMaterial3: true,
+          textTheme: TextTheme(
+            // all > appBar의 메뉴 텍스트
+            titleLarge: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.05,
+                fontWeight: FontWeight.w600,
+                color: Colors.white),
+            // all > 메뉴 설명
+            titleMedium: const TextStyle(
+                fontSize: 18, fontWeight: FontWeight.w600, color: Colors.white),
+            // TD > 투두 컨테이너 > 두 번째 목표
+            bodySmall: const TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: Color(0xffA1A1A1)),
+            // TD > 투두 컨테이너 > 세 번째 목표
+            bodyMedium: const TextStyle(
+                fontSize: 13, fontWeight: FontWeight.w400, color: Colors.white),
+            // MG > 질문 텍스트
+            titleSmall: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.035,
+                fontWeight: FontWeight.w600,
+                color: Colors.white),
           ),
-          // TD > 투두 컨테이너 > 두 번째 목표
-           bodySmall: const TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.w400,
-            color:Color(0xffA1A1A1)
-          ),
-          // TD > 투두 컨테이너 > 세 번째 목표
-           bodyMedium: const TextStyle(
-            fontSize: 13,
-            fontWeight: FontWeight.w400,
-            color:Colors.white
-          ),
-          // MG > 질문 텍스트 
-          titleSmall: TextStyle(
-            fontSize: MediaQuery.of(context).size.width * 0.035,
-            fontWeight: FontWeight.w600,
-            color:Colors.white
-          ),
-        ),
 
-        // all > 인풋박스 스타일
-        inputDecorationTheme: InputDecorationTheme(
-          hintStyle: const TextStyle(
-            color: Color.fromARGB(255, 148, 148, 148),
-            fontSize: 15,
-            fontWeight: FontWeight.w500,
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(7)
-          )
-        ),
-
-        scaffoldBackgroundColor: const Color(0xff262626)
-      
-      )
-      ,
+          // all > 인풋박스 스타일
+          inputDecorationTheme: InputDecorationTheme(
+              hintStyle: const TextStyle(
+                color: Color.fromARGB(255, 148, 148, 148),
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+              ),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(7))),
+          scaffoldBackgroundColor: const Color(0xff262626)),
     );
   }
 }
